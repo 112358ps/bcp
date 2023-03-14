211ba16d4a9b8ebd6c787d38dba4276c4a0dc432
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-14 13:34:37 +0530

    checking number exists inside the input array.

---
+++ 14march2023-2.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file 14march2023-1.cpp
+ *
+ * @brief In taken 10 numbers checking wheather the new input number exists or not.
+
+ *
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num[10];
+    for(int i=0;i<10;i++)
+    {
+        cout << "Enter a number\n";
+        cin >> num[i];
+    }
+    cout << "enter any number\n";
+    int n;
+    cin>>n;
+     for(int i=0;i<10;i++)
+        {
+          if(num[i] == n )
+            {
+                cout <<" exists\n";
+                break ;
+            }
+            else {
+                cout<<"try again\n";
+                }
+        }
+        
+    return 0;
+}
