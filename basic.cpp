b8690d0437a2e91c4de0168510fea8517bb8c2cf
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-14 09:10:37 +0530

    added basic.cpp
    finding area of rectangle.

---
+++ basic.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main(){
+
+
+        int length;
+        length = 0;
+        std::cout << length << "enter number\n" ;
+
+    std::cout << length << " is number\n" ;
+    std::cin >>length;
+    std::cout<<length <<std::endl;
+
+
+        float x(0.5);
+        std::cout << x << "enter number\n" ;
+
+    std::cout << x << " is number\n" ;
+    std::cin >>x;
+    std::cout<<x <<std::endl;
+    char y('s');
+    std::cout << y << "\n";
+    std::cin >> y;
+    std::cout << y << "\n";
+
+
+
+    std::cout <<"enter length \n";
+    int length ;
+    std::cin>>length;
+    std::cout <<length << "\n";
+    std::cout<<"enter bredth\n";
+    int bredth;
+    std::cin>>bredth;
+    std::cout<<bredth<<"\n";
+    std::cout<<"the area is  "             <<length*bredth<< "\n" ;
+
+
+    return 0;
+}
