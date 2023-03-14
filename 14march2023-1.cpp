288ae3858313841134f4719e1a93c7115508f120
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-14 13:32:22 +0530

    printing taken input array numbers

---
+++ 14march2023-1.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file 14march2023-1.cpp
+ *
+ * @brief Take 10 integer inputs from user and store them in an array and print them on screen.
+
+ *
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+int screen()
+{
+    int array[10];
+    for (int i=0;i<10;i++)
+    {
+        cin>>array[i];
+        cout<<array[i]<<"  ";
+       if (i>=10)
+            {
+               break ;
+            }
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    screen();
+    return 0;
+}
