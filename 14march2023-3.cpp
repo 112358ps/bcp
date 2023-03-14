8e79895119e3ca7b9de21d43260b1a1b4b7a50ea
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-14 13:38:28 +0530

    no of different types of integers.
    positive numbers,
    negative numbers,
    zeroes,
    odd and even numbers.

---
+++ 14march2023-3.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file 14march2023-1.cpp
+ *
+ * @brief 
+
+ *
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+int array(){
+    int r[20];
+    int positivenum =0 ;
+    int negativenum=0;
+    int zeroes= 0;
+    int oddnum=0;
+    int evennum=0;
+    for(int i=0;i<20;i++)
+    {
+        cout<<"enter any 20 number\n";
+        cin>>r[i];
+        if(r[i]>0){
+            positivenum++;
+        }
+        else if(r[i]<0)
+        {
+            negativenum++;
+        }
+        else{
+            zeroes++;
+        }
+        if(r[i]%2 == 0)
+        {
+            evennum++;
+
+        }else{
+            oddnum++;
+        }
+    }
+    cout<<positivenum<<"-+numbers \n"<<negativenum<<" -ve numbers\n"<<zeroes<<" noof zeroes\n"<<oddnum<<"  odd numbers\n"<<evennum<<"  even numbers\n";
+    return 0;
+}
+int main()
+{
+    array ();
+return 0;
+}
