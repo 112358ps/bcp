e0a4ee628cc2035bce77ec3892510f43c18c19bf
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-24 13:58:20 +0530

    int to char convertion.

---
+++ march2023/23/23march2023-2.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file 23march2023-2.cpp
+ *
+ * @brief  program  to convert int to character.
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"enter any number\n";
+    cin>>x;
+    cout<<(char) x<<"\n";
+    return 0;
+
+}
