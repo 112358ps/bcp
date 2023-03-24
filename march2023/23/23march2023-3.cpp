69ea88cab629b9b187c34165fc4f2c5d8019979a
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-24 14:14:42 +0530

    float to int

---
+++ march2023/23/23march2023-3.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file 21march2023-9.cpp
+ *
+ * @brief  area using float to int. 
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,area;
+    cout<<"enter lenth and breadth\n";
+    cin>>a>>b;
+    area=a*b;
+    cout<<(int) area<<"\n";
+    return 0;
+
+}
