381b16f127e8bdfb17fe8639c65ff25096d655bd
112358ps <124455106+112358ps@users.noreply.github.com>
2023-03-28 11:06:29 +0530

    length of the string

---
+++ march2023/23/stringsL1-2.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file pointers-1.cpp
+ *
+ * @brief  a program to find the length of the string.
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    string b;
+    cout<<"enter required string\n";
+    cin>>b;
+    cout<<b.length()<<"\n";
+    return 0;
+
+}
