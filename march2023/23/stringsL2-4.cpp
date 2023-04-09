4d7e86ae9e62bb2ed188625779222026ace72262
112358ps <124455106+112358ps@users.noreply.github.com>
2023-04-09 21:41:55 +0530

    concatenate two strings.

---
+++ march2023/23/stringsL2-4.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file stringsL2-4.cpp
+ *
+ * @brief  Write a program to concatenate two strings "Hello" and "World". 
+ * @author 112358ps
+ *
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+    string n,m,o;
+    cout<<"enter required string1:\n";
+    cin>>n;
+    cout<<"enter required string2:\n";
+    cin>>m;
+    o=n+m;
+    cout<<"o="<<o<<"\n";
+    return 0;
+
+}
