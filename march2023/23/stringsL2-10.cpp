5df3e15c51a1aa8ffe5009658335eb276f64a1f6
112358ps <124455106+112358ps@users.noreply.github.com>
2023-04-14 12:13:28 +0530

    reverse string.

--- march2023/23/stringsL2-10.cpp
+++ march2023/23/stringsL2-10.cpp
@@ -1,47 +1,49 @@
 /**
- * @file stringsL2-9.cpp
+ * @file stringsL2-10.cpp
  *
  * @brief Write a program to reverse individual words in a string,
- w     here each word may be delimited by a dot, comma, space or tab, like www.google.com should become www.elgoog.moc.
+    here each word may be delimited by a dot, comma, space or tab, like www.google.com should become www.elgoog.moc.
 
  * @author 112358ps
  *
  */
 #include<iostream>
 using namespace std;
+string r(string a)
+{
+    int n=a.length();
+    for (int i = 0; i < n/2; i++)
+    {
+        char temp=a[i];
+        a[i]=a[n-i-1];
+        a[n-i-1]=temp;
+    }
+    return a;
+}
 string reverse(string o)
 {
-    string p;
+    string s="";
     int present=0,j=0,k,end=0;
     for (int i = 0; i <=o.length(); i++)
     {
         k=0;
         if ((o[i]==' ')||(o[i]==',')||(o[i]=='.')||(o[i]=='\0'))
         {
-            present=i;
-            p=o.substr(j,present-j);
-            end=p.length();
-            cout<<(p=o.substr(j,present-j))<<"\n";
-        //    o[i]=p[p.length()-k];
-        //    k++;
+            string p=o.substr(j,i-j);
+            j=i+1;
+            s=s+r(p)+o[i];
         }
-        j=present+1;
     }
     cout<<"\n";
-    for (int i = 0; i < o.length(); i++)
-    {
-        cout<<o[i];
-    }
-    cout<<"\n";
-    return p;
+    
+    return s;
 }
 int main()
 {
-    string o="www.google.com";
-    // cout<<"enter required string:\n";
-    // getline(cin,o);
-    reverse(o);
-    
+    string o;
+    cout<<"enter required string:\n";
+    getline(cin,o);
+    cout<<reverse(o)<<"\n";
     return 0;
 
 }
